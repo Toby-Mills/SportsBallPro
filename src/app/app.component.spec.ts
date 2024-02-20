18bcf0a8218c89a3ed0616f6ca3534130793aa97
Toby Mills <mills.toby@gmail.com>
2024-02-20 11:32:34 +0200

    Fixes to app name & build config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'WebSportsScraper' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('WebSportsScraper');
+    expect(app.title).toEqual('SportsBallPro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, WebSportsScraper');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SportsBallPro');
   });
 });
