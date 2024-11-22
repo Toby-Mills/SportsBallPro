712e1cf34f42233130608fe5d4f5e912625a6a1c
Toby Mills <toby.mills@spatialdimension.com>
2024-11-22 16:14:43 +0200

    Default display only the active bowlers

--- src/app/bowling-scorecard/bowling-scorecard.component.ts
+++ src/app/bowling-scorecard/bowling-scorecard.component.ts
@@ -1,20 +1,38 @@
 import { CommonModule, NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { BowlingScorecard } from '../models/scorecard';
+import { Component, Input, Pipe, PipeTransform } from '@angular/core';
+import { BowlingScorecard, BowlingScorecardEntry } from '../models/scorecard';
 import { MatchService } from '../services/match.service';
+import { FormsModule } from '@angular/forms';
+
+@Pipe({
+  name: 'visible',
+  standalone: true,
+})
+export class VisibleBowlers implements PipeTransform {
+  transform(bowlers: BowlingScorecardEntry[], allBowlers: boolean): BowlingScorecardEntry[] {
+    return bowlers.filter(bowler => {
+
+      return(allBowlers || (bowler.strikeBowler || bowler.nonStrikeBowler));
+    })
+  }
+}
 
 @Component({
   selector: 'app-bowling-scorecard',
   standalone: true,
   imports: [
     CommonModule,
-    NgFor],
+    FormsModule,
+    NgFor,
+    VisibleBowlers
+  ],
   templateUrl: './bowling-scorecard.component.html',
   styleUrl: './bowling-scorecard.component.css'
 })
 export class BowlingScorecardComponent {
   @Input() teamNumber: 1 | 2 = 1;
   public scorecard: BowlingScorecard = new BowlingScorecard();
+  public allBowlers: boolean = false;
 
   constructor(public matchService: MatchService) { }
 
